ccd5e87028efb8ee44c768e8d9f1745dc36d63f8
dilasha-ghimire <dilashhaa@gmail.com>
2023-06-19 12:52:45 +0545

    Patient UI

--- src/Controller/TheLoginController.java
+++ src/Controller/TheLoginController.java
@@ -52,7 +52,7 @@ public class TheLoginController {
         public boolean checkUser(TheLoginModel user) throws Exception{
         
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/gracedb","root","cerebralcorpse00");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/gracedb","root","");
             String query="select * from registration where email='"+user.getEmail()+"' AND Passwd='"+user.getPassword()+"'";
             try{
                 Statement stmt=conn.createStatement();
