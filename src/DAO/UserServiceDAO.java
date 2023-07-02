8b6bb9add8549904dd6183bcbfd4a6de21015088
Dia505 <dia48261@gmail.com>
2023-07-02 14:15:52 +0545

    User Service FullStack complete

--- src/DAO/UserServiceDAO.java
+++ src/DAO/UserServiceDAO.java
@@ -3,32 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.*;
 import java.sql.PreparedStatement;
 import Model.UserServiceModel;
 import javax.swing.JOptionPane;
 
-public class UserServiceDAO extends DbConnection {
+public class UserServiceDAO {
     public boolean insertData(UserServiceModel user) {
-        PreparedStatement pst = null;
         
         try {
-            try(Connection conn = connect()) {
+            Connection conn = DbConnection.connect();
+            PreparedStatement pst = null;
 
-                String sql = "INSERT INTO user_service (service_name, service_date, patient_id, patient_name, patient_charge) VALUES(?,?,?,?,?)";
+            String sql = "INSERT INTO user_service (service_name, service_date, patient_id, patient_name, service_charge) VALUES(?,?,?,?,?)";
+            pst = conn.prepareStatement(sql);
+                
+            pst.setString(1, user.getService());
+            pst.setString(2, user.getDate());
+            pst.setString(3, user.getPatient_ID());
+            pst.setString(4, user.getPatient_name());
+            pst.setString(5, user.getCharge());
             
-                pst = conn.prepareStatement(sql);
-                pst.setString(1, user.getService());
-                pst.setString(2, user.getDate());
-                pst.setString(3, user.getPatient_ID());
-                pst.setString(4, user.getPatient_name());
-                pst.setString(5, user.getCharge());
-            
-                pst.executeUpdate();
-                JOptionPane.showMessageDialog(null, "Data saved!");
-            }
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Data saved!");
+          
         }
         
         catch(Exception e) {
